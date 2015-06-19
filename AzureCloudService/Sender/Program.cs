c03254d82836384c7e10edc8bdf7d60be4f3ae13
Mercury201 <sakuya_maid@yahoo.co.jp>
2015-06-19 17:38:56 +0900

    送信確認

---
+++ AzureCloudService/Sender/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //キューの名称を設定
+            string queueName = "nortificationqueue";
+
+            //キューの最大サイズ(1GB)とタイムアウト時間(1時間)を設定
+            QueueDescription qd = new QueueDescription(queueName);
+            qd.MaxSizeInMegabytes = 1024;
+            qd.DefaultMessageTimeToLive = new TimeSpan(1, 0, 0);
+
+            //App.Configからサービスバスへの接続情報を読み込む
+            string connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            //サービスバスを管理するためのNamespaceManagerを作成する
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            //キューがなければ作成する
+            if (!namespaceManager.QueueExists(queueName))
+            {
+                namespaceManager.CreateQueue(qd);
+            }
+
+            //キューのクライアントを取得
+            QueueClient client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            //送信するメッセージの本体を生成
+            BrokeredMessage message = new BrokeredMessage("Test Message.");
+            //メッセージを送信
+            client.Send(message);
+            Console.WriteLine("Message(s) sent.");
+            Console.WriteLine("Done, press a key contirue.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AzureCloudService/WebRole1/Controllers/SendController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebRole1
+{
+    public class SendController : ApiController
+    {
+        private QueueClient client;
+
+        public SendController()
+        {
+            var connectionString = "Endpoint=sb://notificationbas.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=du41aFGjHMQTu3Vjn+TOHzHNfyvYFTCbn63BJ8kmRVs=";
+            var queueName = "notificationqueue";
+            this.client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+        }
+
+        public async Task<string> Get()
+        {
+            var message = new BrokeredMessage("Gooner");
+            message.MessageId = Guid.NewGuid().ToString();
+            await this.client.SendAsync(message);
+            return "OK";
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.client.Close();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
